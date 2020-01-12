03ccbacbac59a3908952b001662d2cf370e5cc9b
Mnjae Shin <shinminje@gmail.com>
2020-01-11 20:58:50 -0800

    trivial change

--- nwHackProject/nwHackProject/ContentView.swift
+++ nwHackProject/nwHackProject/ContentView.swift
@@ -1,21 +1,21 @@
-////
-////  ContentView.swift
-////  nwHackProject
-////
-////  Created by Minjae Shin on 2020-01-11.
-////  Copyright © 2020 Minjae Shin. All rights reserved.
-////
 //
-//import SwiftUI
+//  ContentView.swift
+//  nwHackProject
 //
-//struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, World!")
-//    }
-//}
+//  Created by Minjae Shin on 2020-01-11.
+//  Copyright © 2020 Minjae Shin. All rights reserved.
 //
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
